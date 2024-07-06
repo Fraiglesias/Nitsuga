c91ca42742ece54947d59f0a3737119ddbc4a0b4
DenniseAOrtiz <de.ortiz@duocuc.cl>
2024-07-06 01:05:00 -0400

    Se agregan modelos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
+class Colores(models.Model):
+    nombre = models.CharField(max_length=50)
+    disponible = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    disponible = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=500)
+    correo = models.CharField(max_length=100)
+    contrasenia = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    disponible = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+    
