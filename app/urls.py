52be5503eb378fb92588169e72eab917846eeb87
DenniseAOrtiz <de.ortiz@duocuc.cl>
2024-07-06 01:54:49 -0400

    Cambio href categorias

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import home, contacto, categorias, art_escolares, art_varios, bolsos_mochilas, lamparas, libretas_cuadernos, login, nosotros, peluches
+from .views import home, contacto, categorias, artescolares, art_varios, bolsos_mochilas, lamparas, libretas_cuadernos, login, nosotros, peluches
 
 urlpatterns = [
     path('', home, name="home"),
     path('contacto/', contacto, name="contacto"),
     path('categorias/', categorias, name="categorias"),
-    path('art_escolares/', art_escolares, name="articulos escolares"),
-    path('art_varios/', art_varios, name="articulos varios"),
-    path('bolsos_mochilas/', bolsos_mochilas, name="bolsos y mochilas"),
+    path('artescolares/', artescolares, name="artescolares"),
+    path('art_varios/', art_varios, name="art_varios"),
+    path('bolsos_mochilas/', bolsos_mochilas, name="bolsos_mochilas"),
     path('lamparas/', lamparas, name="lamparas"),
-    path('libretas_cuadernos/', libretas_cuadernos, name="libretas y cuadernos"),
+    path('libretas_cuadernos/', libretas_cuadernos, name="libretas_cuadernos"),
     path('login/', login, name="login"),
     path('nosotros/', nosotros, name="nosotros"),
     path('peluches/', peluches, name="peluches"),
