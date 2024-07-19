686b7c4b33fe4d6b570486bac21d2340aa10403c
DenniseAOrtiz <de.ortiz@duocuc.cl>
2024-07-19 15:16:25 -0400

    Se implementan CRUDS faltantes

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, contacto, categorias, art_escolares, art_varios, bolsos_mochilas, lamparas, libretas_cuadernos, login, nosotros, peluches, gestion_home, gestion_usuarios, gestion_roles, gestion_productos, gestion_categorias, valida_login, detalle_categoria, crear_producto, listar_producto, editar_producto, eliminar_producto, crear_pedido
+from .views import home, contacto, categorias, art_escolares, art_varios, bolsos_mochilas, lamparas, libretas_cuadernos, login, nosotros, peluches, gestion_home, gestion_usuarios, gestion_roles, gestion_productos, gestion_categorias, valida_login, detalle_categoria, crear_producto, listar_producto, editar_producto, eliminar_producto, crear_categoria, listar_categoria, editar_categoria, crear_pedido, eliminar_categoria, crear_marca, editar_marca, listar_marca, eliminar_marca, crear_color, listar_color, editar_color, eliminar_color, crear_condicion, listar_condicion, editar_condicion, eliminar_condicion
 
 app_name = 'app'
 
@@ -27,5 +27,21 @@ urlpatterns = [
     path('listar_producto/', listar_producto, name="listar_producto"),
     path('editar_producto/<id>/', editar_producto, name='editar_producto'),
     path('eliminar_producto/<id>/', eliminar_producto, name='eliminar_producto'),
+    path('crear_categoria/', crear_categoria, name="crear_categoria"),
+    path('listar_categoria/', listar_categoria, name="listar_categoria"),
+    path('editar_categoria/<id>/', editar_categoria, name='editar_categoria'),
+    path('eliminar_categoria/<id>/', eliminar_categoria, name='eliminar_categoria'),
+    path('crear_marca/', crear_marca, name="crear_marca"),
+    path('listar_marca/', listar_marca, name="listar_marca"),
+    path('editar_marca/<id>/', editar_marca, name='editar_marca'),
+    path('eliminar_marca/<id>/', eliminar_marca, name='eliminar_marca'),
+    path('crear_color/', crear_color, name="crear_color"),
+    path('listar_color/', listar_color, name="listar_color"),
+    path('editar_color/<id>/', editar_color, name='editar_color'),
+    path('eliminar_color/<id>/', eliminar_color, name='eliminar_color'),
+    path('crear_condicion/', crear_condicion, name="crear_condicion"),
+    path('listar_condicion/', listar_condicion, name="listar_condicion"),
+    path('editar_condicion/<id>/', editar_condicion, name='editar_condicion'),
+    path('eliminar_condicion/<id>/', eliminar_condicion, name='eliminar_condicion'),
     path('crear_pedido/', crear_pedido, name='crear_pedido'),
 ]
